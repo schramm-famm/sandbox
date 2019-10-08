f9699228b589472311416c382a759b36ce851385
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2019-10-08 09:38:26 -0400

    Implemented diffmatchpatch and server sent events * Initialized dependencies * Added new handlers to server for server events and patching * Updated front-end to subscribe to server events and use diffmatchpatch

--- basic_system/server.go
+++ basic_system/server.go
@@ -3,46 +3,165 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/sergi/go-diff/diffmatchpatch"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-var doc string = ""
+var (
+	doc       string
+	dmp       *diffmatchpatch.DiffMatchPatch
+	patchChan chan []diffmatchpatch.Patch
+	broker    *Broker
+)
 
 type RequestBody struct {
-	Snippet string `json:"snippet"`
+	Patch string `json:"patch"`
 }
 
-func snippetHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
+type EventStream struct {
+	Data string `json:"data"`
+}
+
+// A Broker holds open client connections,
+// listens for incoming events on its Notifier channel
+// and broadcast event data to all registered connections
+type Broker struct {
+
+	// Events are pushed to this channel by the main events-gathering routine
+	Notifier chan []byte
+
+	// New client connections
+	newClients chan chan []byte
+
+	// Closed client connections
+	closingClients chan chan []byte
+
+	// Client connections registry
+	clients map[chan []byte]bool
+}
+
+func init() {
+	doc = ""
+	dmp = diffmatchpatch.New()
+	patchChan = make(chan []diffmatchpatch.Patch)
+	broker = NewBroker()
+}
+
+func NewBroker() (broker *Broker) {
+	// Instantiate a broker
+	broker = &Broker{
+		Notifier:       make(chan []byte),
+		newClients:     make(chan chan []byte),
+		closingClients: make(chan chan []byte),
+		clients:        make(map[chan []byte]bool),
+	}
+
+	// Set it running - listening and broadcasting events
+	go broker.listen()
+
+	return
+}
+
+// listen blocks on the different channels of Broker waiting for events
+func (broker *Broker) listen() {
+	for {
+		select {
+		case s := <-broker.newClients:
+			// A new client has connected.
+			// Register their message channel
+			broker.clients[s] = true
+			log.Printf("Client added. %d registered clients", len(broker.clients))
+		case s := <-broker.closingClients:
+			// A client has dettached and we want to
+			// stop sending them messages.
+			delete(broker.clients, s)
+			log.Printf("Removed client. %d registered clients", len(broker.clients))
+		case event := <-broker.Notifier:
+			// We got a new event from the outside!
+			// Send event to all connected clients
+			for clientMessageChan, _ := range broker.clients {
+				clientMessageChan <- event
+			}
+		}
+	}
+}
+
+// subscribeHandler registers a new connection and maintains it to send events
+// back to it
+func subscribeHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if server sent events are supported
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-	if r.Method == "OPTIONS" {
-		return
+	// Each connection registers its own message channel with the Broker's
+	// connections registry
+	messageChan := make(chan []byte)
+
+	// Signal the broker that we have a new connection
+	broker.newClients <- messageChan
+
+	// Remove this client from the map of connected clients
+	// when this handler exits.
+	defer func() {
+		broker.closingClients <- messageChan
+	}()
+
+	// Listen to connection close and un-register messageChan
+	notify := w.(http.CloseNotifier).CloseNotify()
+
+	go func() {
+		<-notify
+		broker.closingClients <- messageChan
+	}()
+
+	// Block on messageChan to wait for next event
+	for m := range messageChan {
+		fmt.Fprintf(w, "data:%s\n\n", m)
+		// Clear buffer
+		flusher.Flush()
 	}
+}
 
+// patchHandler parses patches sent from a client and applies it to the global
+// doc
+func patchHandler(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Println("Failed to read request body:", err)
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
 		return
 	}
 
-	data := RequestBody{}
-	err = json.Unmarshal(body, &data)
+	patchBody := RequestBody{}
+	err = json.Unmarshal(reqBody, &patchBody)
 	if err != nil {
-		log.Println("Failed to parse JSON:", err)
+		http.Error(w, "Failed to parse request body", http.StatusBadRequest)
 		return
 	}
 
-	fmt.Println(data)
-	doc += data.Snippet
+	patches, err := dmp.PatchFromText(patchBody.Patch)
+	if err != nil {
+		http.Error(w, "Failed to parse patches", http.StatusBadRequest)
+		return
+	}
+
+	// Send patches to patchChan to be applied to doc
+	patchChan <- patches
+
+	// Send to other connections
+	broker.Notifier <- reqBody
 }
 
 func stateHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
 	fmt.Fprintf(w, doc)
 }
 
@@ -50,7 +169,15 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 
 	http.Handle("/", fs)
-	http.HandleFunc("/snippet/", snippetHandler)
+	http.HandleFunc("/subscribe", subscribeHandler)
+	http.HandleFunc("/patch/", patchHandler)
 	http.HandleFunc("/state/", stateHandler)
+
+	go func() {
+		for patches := range patchChan {
+			doc, _ = dmp.PatchApply(patches, doc)
+		}
+	}()
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- basic_system/static/script.js
+++ basic_system/static/script.js
@@ -1,34 +1,60 @@
+let dmp = null;
+let prevPatch = "";
+
 document.addEventListener("DOMContentLoaded", async function() {
+  dmp = new diff_match_patch();
   const doc = document.querySelector("#doc");
 
-  updateText(doc);
+  doc.value = await getState().catch(err => console.log(err));
+
+  // On focus, save the value of the doc before any input
+  doc.addEventListener("focus", () => doc.prevValue = doc.value)
 
   doc.addEventListener("input", (event) => {
-    console.log(event);
-
-    if (event.inputType === "insertText") {
-      sendSnippet(event.data);
-    } else if (event.inputType === "insertLineBreak") {
-      sendSnippet("\n");
-    }
+    sendPatch(doc.prevValue, doc.value);
+    // Update previous value of the doc
+    doc.prevValue = doc.value;
   });
 
-  console.log("Test");
+  let client = new EventSource("/subscribe");
+  client.onmessage = getPatch;
+  client.onerror = (err) => {
+    console.error("EventSource failed: ", err);
+  }
 });
 
-function sendSnippet(snippet) {
-  let body = { snippet };
-  let blob = new Blob([JSON.stringify(body)], { type: "application/json" });
+// getPatch handles EventSource events and applies the patch to the doc
+function getPatch(e) {
+  let body = JSON.parse(e.data);
+  // Patch doesn't get applied if it's the same as the last patch that the
+  // client sent
+  if (body.patch && body.patch != prevPatch) {
+    let patches = dmp.patch_fromText(body.patch);
+    [doc.value] = dmp.patch_apply(patches, doc.value);
+    doc.prevValue = doc.value;
+    prevPatch = "";
+  }
+}
+
+// sendPatch sends the patches between the prev text and curr text to the server
+// to be applied to the global doc
+function sendPatch(prev, curr) {
+  let patch = dmp.patch_toText(dmp.patch_make(prev, curr))
+  let body = { patch };
 
   const req = new XMLHttpRequest();
-  const url = "/snippet/";
+  const url = "/patch/";
   req.open("POST", url);
-
-  req.send(blob);
+  req.setRequestHeader("Content-Type", "application/json");
+  req.send(JSON.stringify(body));
 
   req.onreadystatechange = (event) => {
-      console.log(req.responseText);
+    if (req.readState === 4 && req.status !== 200) {
+      console.log(req.response);
+    }
   };
+
+  prevPatch = patch;
 }
 
 async function getState() {
@@ -40,15 +66,13 @@ async function getState() {
 
   return new Promise((resolve, reject) => {
     req.onreadystatechange = (event) => {
-      if(req.readyState === 4 && req.status === 200) {
-        console.log(req.responseText);
-        resolve(req.responseText);
+      if (req.readyState === 4) {
+        if (req.status === 200) {
+          resolve(req.responseText);
+        } else {
+          reject(req.responseText);
+        }
       }
     };
   });
-}
-
-async function updateText(doc) {
-  doc.value = await getState();
-  setTimeout(() => updateText(doc), 500)
 }
